b608cd85101a077d184c14edd678068a9a4a72f1
Lpokimo <61668752+Lpokimo@users.noreply.github.com>
2023-03-23 21:53:44 -0400

    first commit

---
+++ config/nvim/init.vim
@@ -0,0 +1,11 @@
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+ Plugin 'joshdick/onedark.vim'
+call vundle#end()
+colorscheme onedark
+
+set hlsearch   
+set number
+set ttyfast
+set syntax
+set mouse=
--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo mv make.conf /etc/portage/make.conf
+sudo mv config/make.conf /etc/portage/make.conf
 
 sudo emerge -q doas
 
 sudo emerge -q zsh
-mv .zshrc ~/.zshrc
-mv -r .zsh ~/.zsh
+mv config/.zshrc ~/.zshrc
+mv -r config/.zsh ~/.zsh
 
 sudo emerge -q librewolf
 
@@ -19,19 +19,19 @@ mkdir ~/Pictures
 mkdir ~/Pictures/Wallpapers
 
 
-mv bspwm ~/.config/bspwm
-mv sxhkd ~/.config/sxhkd
+mv config/bspwm ~/.config/bspwm
+mv config/sxhkd ~/.config/sxhkd
 mv static/colors.sh ~/.cache/wal/colors.sh
 mv static/nasa.png ~/Pictures/Wallpapers/nasa.png
 
 
 
 sudo emerge -q polybar
-mv polybar ~/.config/polybar
+mv config/polybar ~/.config/polybar
 
 
 sudo emerge -q neovim
-mv nvim ~/.config/nvim
+mv config/nvim ~/.config/nvim
 
 git clone https://git.suckless.org/st
 
